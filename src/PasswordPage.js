9b3b24a1cafcc6433e61c00310a74edeef4909dc
aashish-65 <aashishprasad65@gmail.com>
2024-11-05 02:23:11 +0530

    Improve PasswordPage.js

--- src/PasswordPage.js
+++ src/PasswordPage.js
@@ -1,24 +1,55 @@
 // PasswordPage.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './PasswordPage.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./PasswordPage.css";
 import CodeNESTTitle from "./assets/svg/CodeNESTTitle.svg";
 import CodeNESTSlogan from "./assets/svg/Slogan.svg";
 import CodeNESTIcon from "./assets/svg/icon.svg";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  IconButton,
+  TextField,
+  InputAdornment,
+  InputLabel,
+  Input
+} from "@mui/material";
 
 function PasswordPage({ onVerify }) {
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  //   const [password, setPassword] = useState("");
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+  });
+  const [error, setError] = useState("");
+  //   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handlePasswordSubmit = () => {
-    const correctPassword = process.env.SCAN_PASSWORD; // Replace with your desired password
-    if (password === correctPassword) {
+    const correctPassword = "scan@codenest"; //process.env.REACT_APP_PASSWORD // Replace with your desired password
+    if (values.password === correctPassword) {
       onVerify();
-      navigate('/scan');
+      navigate("/scan");
     } else {
-      setError('Incorrect password. Please try again.');
+      setError("Incorrect password. Please try again.");
     }
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handlePasswordChange = (prop) => (event) => {
+    setValues({
+      ...values,
+      [prop]: event.target.value,
+    });
   };
 
   return (
@@ -42,13 +73,25 @@ function PasswordPage({ onVerify }) {
       </div>
       <div className="password-form">
         <h2>Enter Password to Access Scan</h2>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+        <Input
+          id="password"
+          type={values.showPassword ? "text" : "password"}
+          onChange={handlePasswordChange("password")}
+          value={values.password}
           placeholder="Password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                id="show-password"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
         />
-        <button onClick={handlePasswordSubmit}>Submit</button>
+        <button className="submit-button" onClick={handlePasswordSubmit}>Submit</button>
         {error && <p>{error}</p>}
       </div>
     </div>
