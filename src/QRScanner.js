d607cb9a3a45ecc81e8bb282899a305564af0c14
aashish-65 <aashishprasad65@gmail.com>
2024-11-06 00:06:53 +0530

    Update camera setting in Scanner page

--- src/QRScanner.js
+++ src/QRScanner.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 import './QRScanner.css';
@@ -6,7 +6,19 @@ import './QRScanner.css';
 function QRScanner({ onAuthorized }) {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [deviceId, setDeviceId] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Enumerate video input devices
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+      
+      // Try to find a device without 'wide' in the label, usually the regular camera
+      const regularCamera = videoDevices.find(device => !device.label.toLowerCase().includes('wide'));
+      setDeviceId(regularCamera ? regularCamera.deviceId : videoDevices[0].deviceId);
+    });
+  }, []);
 
   const handleScan = async (error, result) => {
     if (result) {
@@ -39,12 +51,14 @@ function QRScanner({ onAuthorized }) {
     <div className="qr-scanner-container">
       {loading && <p className="loading-message-scanner">Loading...</p>}
       {error && <p className="error-message-scanner">{error}</p>}
-      <BarcodeScannerComponent
-        onUpdate={handleScan}
-        facingMode="environment"
-        delay={500}
-        style={{ width: '100%', height: 'auto' }}
-      />
+      {deviceId && (
+        <BarcodeScannerComponent
+          onUpdate={handleScan}
+          delay={500}
+          constraints={{ video: { deviceId: { exact: deviceId } } }}
+          style={{ width: '100%', height: 'auto' }}
+        />
+      )}
       <button className="back-button-scanner" onClick={() => navigate('/')}>
         Back to Home
       </button>
