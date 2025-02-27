7cd12649afe0e2c568bb90fcdb4e93a052c39421
aashish-65 <aashishprasad65@gmail.com>
2025-02-28 01:25:20 +0530

    Add new event

--- src/sections/UpcomingEvents.js
+++ src/sections/UpcomingEvents.js
@@ -5,13 +5,51 @@ const UpcomingEvents = () => {
   return (
     <section id="events" className="events-section">
       <h2>Upcoming Events</h2>
-      <section className="event-section">
+      {/* <section className="event-section">
         <div className="event-overview">
           <h3>No Events</h3>
         </div>
+      </section> */}
+      <section className="logo-making event-section">
+        <div className="event-overview">
+          <h3>
+            📢 Building and Securing Full Stack Apps with Firebase & React
+          </h3>
+          <p>
+            🎤 <strong>Speaker:</strong> Debajit Mallick 
+          </p>
+          <p>👨‍💻 Software Engineer @
+            P360 | Organizer @ GDG Siliguri</p>
+
+          <p>
+            <strong>Debajit Mallick</strong>, a passionate Software Engineer
+            with <strong>3+ years</strong> of experience, will guide you through
+            building secure full-stack applications with Firebase & React! With
+            his rich experience, including winning{" "}
+            <strong>
+              Smart India Hackathon 2020, mentoring SIH 2022 winners
+            </strong>
+            , and speaking at prestigious events like{" "}
+            <strong>FrontendDays Siliguri</strong> &{" "}
+            <strong>Azure DevDay</strong>, this session is bound to be 🔥!
+          </p>
+          <p>
+            <strong>Date:</strong> February 28, 2025
+          </p>
+          <p>
+            <strong>Time:</strong> 10:00 AM
+          </p>
+          <p>
+            <strong>Location:</strong> NSHM Knowledge Campus, Durgapur
+          </p>
+
+          {/* <Link to="/event/logo-making">
+            <button>View Details</button>
+          </Link> */}
+        </div>
       </section>
       <p>Join us for workshops, hackathons, and more!</p>
-    </section> 
+    </section>
   );
 };
 
